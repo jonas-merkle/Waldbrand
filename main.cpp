2c0e9b6d22ae8b9156f7fec7ffcdeb161386a512
Jonas Merkle <jonas_merkle@ymail.com>
2018-05-06 10:51:14 +0200

    Wald erzeugen, ausgeben und anzünden implementiert.

--- main.cpp
+++ main.cpp
@@ -26,33 +26,45 @@ void printForest(forest_t *forest) {
 
     for (int y = 0; y < FOREST_HIGHT; y++) {
         for (int x = 0; x < FOREST_WIDTH; x ++) {
-            cout << forest->area[y][x] << "  ";
+            printf(" %c ", forest->area[y][x]);
         }
-        cout << endl;
+        printf("\n");
     }
-    cout << endl;
-
+    printf("\n");
 }
 
 /*
  * Function which generates a random forest.
  */
-void generateForest(forest_t *forest, float randFact) {
-
+void generateForest(forest_t *forest, float dencity) {
+    for (int y = 0; y < FOREST_HIGHT; y++) {
+        for (int x = 0; x < FOREST_WIDTH; x ++) {
+            if (((dencity * 10) - 1) < (rand() % 10)) {
+                forest->area[y][x] = EMPTY;
+            }
+            else {
+                forest->area[y][x] = TREE;
+            }
+        }
+    }
 }
 
 /*
  * Function to start the forest fires at one point.
  */
 void setFirePoint(forest_t *forest) {
-
+    forest->is_buring = true;
+    forest->area[0][0] = FIRE;
 }
 
 /*
  * Function to start the forest fires at the howl first row.
  */
 void setFireRow(forest_t *forest) {
-
+    forest->is_buring = true;
+    for (int x = 0; x < FOREST_WIDTH; ++x) {
+        forest->area[0][x] = FIRE;
+    }
 }
 
 /*
@@ -66,7 +78,16 @@ void makeItBurn(forest_t *forest) {
  * Main-Function...
  */
 int main() {
-    cout << "Hello, World!" << endl;
+
+    // init the seed for rand()
+    srand(time(NULL));
+
+
+    forest_t wald;
+    generateForest(&wald, 0.9);
+    printForest(&wald);
+    setFirePoint(&wald);
+    printForest(&wald);
 
     return 0;
 }
